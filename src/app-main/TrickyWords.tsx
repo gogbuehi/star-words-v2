e5f6e9ac041ad1afb4b69eafd2a2ce9313b9211e
Goodwin Ogbuehi <goodwino@webother.co.uk>
2022-09-28 13:35:46 +0100

    Adding rounds, so we know when we've gone through all the words

--- src/app-main/TrickyWords.tsx
+++ src/app-main/TrickyWords.tsx
@@ -4,13 +4,18 @@ import {useState} from "react";
 const TrickyWords = () => {
   const [wordIndex, setWordIndex] = useState(0);
   const [randomWordList, setRandomWordList] = useState(WORD_LIST);
+  const [wordRound, setWordRound] = useState(1);
+  // const [trickyWords, setTrickyWords] = useState([]);
+
+  const WordBoxComponent = wordRound === 1 ? WordBox : WordBoxRound2;
   return <div>
     <h1>Tricky Words</h1>
     <div>
       <h2>What's the word?</h2>
-      <WordBox
-      onClick={() => { nextWord(wordIndex, setWordIndex, setRandomWordList); }}
-      >{randomWordList[wordIndex]}</WordBox>
+      <WordBoxComponent
+      onClick={() => { nextWord(wordIndex, wordRound, setWordIndex, setRandomWordList, setWordRound); }}
+      >{randomWordList[wordIndex]}</WordBoxComponent>
+      <TrickyWordButton>TRICKY</TrickyWordButton>
     </div>
   </div>
 }
@@ -28,7 +33,28 @@ const WordBox = styled.div`
   font-size: xxx-large;
   width: fit-content;
   display: inline-block;
-;
+`;
+
+const WordBoxRound2 = styled(WordBox)`
+  border: solid 3px forestgreen;
+  color: #e0d080;
+  background-color: #002030;
+`;
+
+const TrickyWordButton = styled.button`
+  border: solid 1px #800020 ;
+  border-radius: 5px;
+  padding: 20px;
+  margin: 10px;
+  text-align: center;
+  vertical-align: center;
+  color: #e0d0e0;
+  background-color: #303030;
+  
+  font-weight: bold;
+  font-size: x-large;
+  width: fit-content;
+
 `;
 
 const WORD_LIST = [
@@ -66,13 +92,16 @@ const WORD_LIST = [
 
 const nextWord = (
   index: number,
+  currentRound: number,
   updateIndexCallback: (num: number) => any,
-  updateWordListCallback: (wordList: any[]) => any
+  updateWordListCallback: (wordList: any[]) => any,
+  updateRoundCallback: (num: number) => any
 ) => {
   const modIndex = (index+1) % WORD_LIST.length;
   if (modIndex === 0) {
     const wordList = randomizeWordList();
     updateWordListCallback(wordList);
+    updateRoundCallback(currentRound + 1)
 
   }
 
